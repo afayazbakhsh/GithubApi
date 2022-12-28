064d42c214b52a8441f9158980f0c0318139f7b6
amirfayazbakhsh <a.fayazbakhsh1@gmail.com>
2022-12-28 17:56:25 +0330

    feat: a service class to handle requests to github

---
+++ app/Services/GithubRequestsService.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Services;
+
+use App\Actions\HandleRequests;
+use Illuminate\Http\Request;
+
+class GithubRequestsService
+{
+    private $request;
+
+    public function __construct(HandleRequests $request){
+
+        return $this->request = $request;
+    }
+
+        // Get Authenticated User
+    public function getAuthenticatedUser($token){
+
+        return $this->request->handle($token, 'user','get');
+    }
+
+    //     // Update Authenticated User
+    // public function updateUser($token){
+
+    //     return $this->request->handle($token,'user/','patch');
+    // }
+
+    //     // Get All Users In Github
+    // public function users(){
+
+    //     $param = [
+    //         'since' => 100,
+    //         'per_page' => 50,
+    //     ];
+
+    //     return $this->request->handle('users','get' , $param);
+    // }
+
+    //     // Get User By Username
+    // public function getUser(Request $request){
+
+    //     return $this->request->handle('users/'.$request->username,'get');
+    // }
+
+    //     // Get Repositories
+    // public function getRepo(){
+
+    //    return $this->request->handle('user/repos','get');
+    // }
+}
