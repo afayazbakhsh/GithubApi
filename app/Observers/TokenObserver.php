a532f64ece5a7fab1090f8408aa9daeccbdc3d6a
amirfayazbakhsh <a.fayazbakhsh1@gmail.com>
2022-12-28 17:57:28 +0330

    feat: save details of user after create token

---
+++ app/Observers/TokenObserver.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Observers;
+
+use App\Jobs\GetUserGithubProfileJob;
+use App\Models\Token;
+
+class TokenObserver
+{
+    /**
+     * Handle the Token "created" event.
+     *
+     * @param  \App\Models\Token  $token
+     * @return void
+     */
+    public function created(Token $token)
+    {
+        GetUserGithubProfileJob::dispatch($token);
+    }
+
+    /**
+     * Handle the Token "updated" event.
+     *
+     * @param  \App\Models\Token  $token
+     * @return void
+     */
+    public function updated(Token $token)
+    {
+        //
+    }
+
+    /**
+     * Handle the Token "deleted" event.
+     *
+     * @param  \App\Models\Token  $token
+     * @return void
+     */
+    public function deleted(Token $token)
+    {
+        //
+    }
+
+    /**
+     * Handle the Token "restored" event.
+     *
+     * @param  \App\Models\Token  $token
+     * @return void
+     */
+    public function restored(Token $token)
+    {
+        //
+    }
+
+    /**
+     * Handle the Token "force deleted" event.
+     *
+     * @param  \App\Models\Token  $token
+     * @return void
+     */
+    public function forceDeleted(Token $token)
+    {
+        //
+    }
+}
