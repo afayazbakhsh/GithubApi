a6c4ab412f34053359a0b17cdff2a4c84667fd4e
amirfayazbakhsh <a.fayazbakhsh1@gmail.com>
2022-12-28 17:55:29 +0330

    build: github profiles entity and table

---
+++ database/migrations/2022_12_28_140426_create_github_profiles_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('github_profiles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('token_id')->constrained();
+            $table->string('name');
+            $table->string('username')->unique();
+            $table->string('avatar');
+            $table->integer('followers_count')->nullable();
+            $table->integer('following_count')->nullable();
+            $table->string('bio')->nullable();
+            $table->string('email')->nullable();
+            $table->string('public_repos_count')->nullable();
+            $table->string('link');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('github_profiles');
+    }
+};
