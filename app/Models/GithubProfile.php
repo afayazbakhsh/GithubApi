a417ac7b7450b4f63b198f384aae584d76ae7051
amirfayazbakhsh <a.fayazbakhsh1@gmail.com>
2023-01-02 08:52:04 +0330

    feat: relation method wth token

--- app/Models/GithubProfile.php
+++ app/Models/GithubProfile.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class GithubProfile extends Model
 {
     use HasFactory;
+
+
+    protected $fillable = [
+        'name',
+        'username',
+        'avatar',
+        'followers_count',
+        'following_count',
+        'bio',
+        'email',
+        'public_repos_count',
+        'link'
+    ];
+
+
+    public function token(){
+
+        return $this->belongsTo(Token::class);
+    }
 }
